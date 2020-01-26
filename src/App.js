3e5f2f6901527a6ecffa426754fcbe6668cfbde4
eclectic-coding <chuck@eclecticsaddlebag.com>
2020-01-26 07:28:30 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 import Timer from './Timer'
 
@@ -9,16 +9,11 @@ class App extends Component {
     timerIDs: []
   }
 
-
   //Your code here:
 
-
-
-
-
-
-
-
+  componentDidMount() {
+    this.handleAddTimer()
+  }
 
   // No need to modify anything in render or the class methods below
   // Unless, of course, you're curious about how it all works
@@ -34,7 +29,7 @@ class App extends Component {
         </div>
 
       </div>
-    );
+    )
   }
 
   // returns array of components written in JSX, mapped from this.state.timerIDs
@@ -45,7 +40,7 @@ class App extends Component {
   // adds a random number for timer ID
   handleAddTimer = () => {
     this.setState(prevState => ({
-      timerIDs: [...prevState.timerIDs, Math.floor(Math.random()*1000)]
+      timerIDs: [...prevState.timerIDs, Math.floor(Math.random() * 1000)]
     }))
   }
 
@@ -56,7 +51,6 @@ class App extends Component {
     }))
   }
 
-
 }
 
-export default App;
+export default App
